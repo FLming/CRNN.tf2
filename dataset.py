e323d92ef33c2b90b7d82bc646f95564cd4b7c88
FLming <yiminghd@foxmail.com>
2020-01-13 04:35:31 +0000

    format code

--- dataset.py
+++ dataset.py
@@ -15,56 +15,61 @@ class OCRDataLoader():
                  shuffle=False, 
                  repeat=1):
         
-        imgpaths, labels = self.read_imagepaths_and_labels(annotation_paths)
+        img_paths, labels = self._read_img_paths_and_labels(annotation_paths)
         self.batch_size = batch_size
         self.image_width = image_width
         self.image_height = image_height
-        self.size = len(imgpaths)
+        self.size = len(img_paths)
 
-        file_init = tf.lookup.TextFileInitializer(table_path, 
-                                                  tf.string, 
-                                                  tf.lookup.TextFileIndex.WHOLE_LINE,
-                                                  tf.int64,
-                                                  tf.lookup.TextFileIndex.LINE_NUMBER)
+        file_init = tf.lookup.TextFileInitializer(
+            table_path, 
+            tf.string, 
+            tf.lookup.TextFileIndex.WHOLE_LINE,
+            tf.int64,
+            tf.lookup.TextFileIndex.LINE_NUMBER)
         # Default value for blank label
-        self.table = tf.lookup.StaticHashTable(initializer=file_init, default_value=blank_index)
+        self.table = tf.lookup.StaticHashTable(
+            initializer=file_init, 
+            default_value=blank_index)
 
-        dataset = tf.data.Dataset.from_tensor_slices((imgpaths, labels))
+        dataset = tf.data.Dataset.from_tensor_slices((img_paths, labels))
         if shuffle:
             dataset = dataset.shuffle(buffer_size=self.size)
         dataset = dataset.map(self._decode_and_resize)
+        # Experimental function.
         # Ignore the errors e.g. decode error or invalid data.
-        dataset = dataset.apply(tf.data.experimental.ignore_errors()) 
-        # Pay attention to the location of the batch function.
+        dataset = dataset.apply(tf.data.experimental.ignore_errors())
         dataset = dataset.batch(batch_size)
         dataset = dataset.map(self._convert_label)
         dataset = dataset.repeat(repeat)
+        dataset = dataset.prefetch(tf.data.experimental.AUTOTUNE)
 
         self.dataset = dataset
 
-    def read_imagepaths_and_labels(self, annotation_path):
-        """Read txt file to get image paths and labels."""
+    def _read_img_paths_and_labels(self, annotation_paths):
+        """Read annotation files to get image paths and labels."""
 
-        imgpaths = []
+        img_paths = []
         labels = []
-        for annpath in annotation_path.split(','):
-            # If you use your own dataset, maybe you should change the parse code below.
-            annotation_folder = os.path.dirname(annpath)
-            with open(annpath) as f:
-                content = np.array([line.strip().split() for line in f.readlines()])
-            imgpaths_local = content[:, 0]
+        for annotation_path in annotation_paths.split(','):
+            # If you use your own dataset, maybe you should change the 
+            # parse code below.
+            annotation_folder = os.path.dirname(annotation_path)
+            with open(annotation_path) as f:
+                content = np.array(
+                    [line.strip().split() for line in f.readlines()])
+            part_img_paths = content[:, 0]
             # Parse MjSynth dataset. format: XX_label_XX.jpg XX
             # URL: https://www.robots.ox.ac.uk/~vgg/data/text/            
-            labels_local = [line.split("_")[1] for line in imgpaths_local]
-
+            part_labels = [line.split("_")[1] for line in part_img_paths]
             # Parse example dataset. format: XX.jpg label
-            # labels_local = content[:, 1]
+            # part_labels = content[:, 1]
+            part_img_paths = [os.path.join(annotation_folder, line)
+                              for line in part_img_paths]
+            img_paths.extend(part_img_paths)
+            labels.extend(part_labels)
 
-            imgpaths_local = [os.path.join(annotation_folder, line) for line in imgpaths_local]
-            imgpaths.extend(imgpaths_local)
-            labels.extend(labels_local)
-
-        return imgpaths, labels
+        return img_paths, labels
 
     def _decode_and_resize(self, filename, label):
         image = tf.io.read_file(filename)
@@ -117,26 +122,39 @@ def map_to_chars(inputs, table, blank_index=0, merge_repeated=False):
 
 def map_and_count(decoded, Y, mapper, blank_index=0, merge_repeated=False):
     decoded = tf.sparse.to_dense(decoded[0], default_value=blank_index).numpy()
+    decoded = map_to_chars(decoded, mapper, blank_index=blank_index, 
+                           merge_repeated=merge_repeated)
     Y = tf.sparse.to_dense(Y, default_value=blank_index).numpy()
-    decoded = map_to_chars(decoded, mapper, blank_index=blank_index, merge_repeated=merge_repeated)
-    Y = map_to_chars(Y, mapper, blank_index=blank_index, merge_repeated=merge_repeated)
-    count = 0
+    Y = map_to_chars(Y, mapper, blank_index=blank_index, 
+                     merge_repeated=merge_repeated)
+    cnt = 0
     for y_pred, y in zip(decoded, Y):
         if y_pred == y:
-            count += 1
-    return count
-
+            cnt += 1
+    return cnt
 
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser(description="Process some integers.")
-    parser.add_argument("-p", "--annotation_paths", type=str, help="The paths of annnotation file.")
-    parser.add_argument("-t", "--table_path", type=str, help="The path of table file.")
+    import time
+
+    import arg
+
+    parser = argparse.ArgumentParser(parents=[arg.parser])
+    parser.add_argument("-p", "--annotation_paths", type=str, 
+                        help="The paths of annnotation file.")
     args = parser.parse_args()
 
-    dataloader = OCRDataLoader(args.annotation_paths, 32, 100, table_path=args.table_path, shuffle=True, batch_size=2)
+    dataloader = OCRDataLoader(
+        args.annotation_paths, 
+        args.image_height, 
+        args.image_width, 
+        table_path=args.table_path, 
+        shuffle=True, 
+        batch_size=32)
     print("Total have {} data".format(len(dataloader)))
     print("Element spec is: {}".format(dataloader().element_spec))
-    for image, label in dataloader().take(1):
-        label = tf.sparse.to_dense(label).numpy()
-        print("The image's shape: {}\nlabel is \n{}".format(image.shape, label))
+    start_time = time.perf_counter()
+    for i in range(2):
+        for image, label in dataloader().take(1000):
+            pass
+    print("Execution time:", time.perf_counter() - start_time)
--- train.py
+++ train.py
@@ -4,19 +4,27 @@ import time
 import numpy as np
 import tensorflow as tf
 
-import base_arg
+import arg
 from model import CRNN
 from dataset import OCRDataLoader, map_and_count
 
-parser = argparse.ArgumentParser(parents=[base_arg.parser])
-parser.add_argument("-ta", "--train_annotation_paths", type=str, required=True, help="The path of training data annnotation file.")
-parser.add_argument("-va", "--val_annotation_paths", type=str, help="The path of val data annotation file.")
-parser.add_argument("-b", "--batch_size", type=int, default=128, help="Batch size.")
-parser.add_argument("-e", "--epochs", type=int, default=5, help="Num of epochs to train.")
-parser.add_argument("-r", "--learning_rate", type=float, default=0.001, help="Learning rate.")
-parser.add_argument("--checkpoint", type=str, help="The checkpoint path. (Restore)")
-parser.add_argument("--max_to_keep", type=int, default=5, help="Max num of checkpoint to keep.")
-parser.add_argument("--save_freq", type=int, default=1, help="Save and validate interval.")
+parser = argparse.ArgumentParser(parents=[arg.parser])
+parser.add_argument("-ta", "--train_annotation_paths", type=str, required=True, 
+                    help="The path of training data annnotation file.")
+parser.add_argument("-va", "--val_annotation_paths", type=str, 
+                    help="The path of val data annotation file.")
+parser.add_argument("-b", "--batch_size", type=int, default=256, 
+                    help="Batch size.")
+parser.add_argument("-e", "--epochs", type=int, default=5, 
+                    help="Num of epochs to train.")
+parser.add_argument("-r", "--learning_rate", type=float, default=0.001, 
+                    help="Learning rate.")
+parser.add_argument("--checkpoint", type=str, 
+                    help="The checkpoint path. (Restore)")
+parser.add_argument("--max_to_keep", type=int, default=5, 
+                    help="Max num of checkpoint to keep.")
+parser.add_argument("--save_freq", type=int, default=1, 
+                    help="Save and validate interval.")
 args = parser.parse_args()
 
 with open(args.table_path, "r") as f:
@@ -24,98 +32,123 @@ with open(args.table_path, "r") as f:
 NUM_CLASSES = len(INT_TO_CHAR)
 BLANK_INDEX = NUM_CLASSES - 1 # Make sure the blank index is what.
 
+def dataloader():
+    train_dl = OCRDataLoader(
+        args.train_annotation_paths, 
+        args.image_height, 
+        args.image_width, 
+        table_path=args.table_path,
+        blank_index=BLANK_INDEX,
+        shuffle=True, 
+        batch_size=args.batch_size)
+    print(f"Num of training samples: {len(train_dl)}")
+    if args.val_annotation_paths:
+        val_dl = OCRDataLoader(
+            args.val_annotation_paths,
+            args.image_height,
+            args.image_width,
+            table_path=args.table_path,
+            blank_index=BLANK_INDEX,
+            batch_size=args.batch_size)
+        print(f"Num of val samples: {len(val_dl)}")
+    else:
+        val_dl = None
+    print(f"Num of classes: {NUM_CLASSES}")
+    print(f"Blank index is {BLANK_INDEX}")
+    return train_dl, val_dl
+
 @tf.function
-def train_one_step(model, X, Y, optimizer):
+def train_one_step(model, optimizer, x, y):
     with tf.GradientTape() as tape:
-        y_pred = model(X, training=True)
-        loss = tf.nn.ctc_loss(labels=Y,
-                              logits=tf.transpose(y_pred, perm=[1, 0, 2]),
-                              label_length=None,
-                              logit_length=[y_pred.shape[1]]*y_pred.shape[0],
-                              blank_index=BLANK_INDEX)
+        logits = model(x, training=True)
+        logit_length = tf.fill([tf.shape(logits)[0]], tf.shape(logits)[1])
+        loss = tf.nn.ctc_loss(
+            labels=y,
+            logits=logits,
+            label_length=None,
+            logit_length=logit_length,
+            logits_time_major=False,
+            blank_index=BLANK_INDEX)
         loss = tf.reduce_mean(loss)
     grads = tape.gradient(loss, model.trainable_variables)
-    optimizer.apply_gradients(grads_and_vars=zip(grads, model.trainable_variables))
+    optimizer.apply_gradients(zip(grads, model.trainable_variables))
     return loss
 
 @tf.function
-def val_one_step(model, X, Y):
-    y_pred = model(X, training=False)
-    loss = tf.nn.ctc_loss(labels=Y,
-                          logits=tf.transpose(y_pred, perm=[1, 0, 2]),
-                          label_length=None,
-                          logit_length=[y_pred.shape[1]]*y_pred.shape[0],
-                          blank_index=BLANK_INDEX)
+def val_one_step(model, x, y):
+    logits = model(x, training=False)
+    logit_length = tf.fill([tf.shape(logits)[0]], tf.shape(logits)[1])
+    loss = tf.nn.ctc_loss(
+        labels=y,
+        logits=logits,
+        label_length=None,
+        logit_length=logit_length,
+        logits_time_major=False,
+        blank_index=BLANK_INDEX)
     loss = tf.reduce_mean(loss)
-    decoded, neg_sum_logits = tf.nn.ctc_greedy_decoder(inputs=tf.transpose(y_pred, perm=[1, 0, 2]),
-                                                       sequence_length=[y_pred.shape[1]]*y_pred.shape[0],
-                                                       merge_repeated=True)
-    return decoded, loss
+    decoded, neg_sum_logits = tf.nn.ctc_greedy_decoder(
+        inputs=tf.transpose(logits, perm=[1, 0, 2]),
+        sequence_length=logit_length,
+        merge_repeated=True)
+    return loss, decoded
 
 if __name__ == "__main__":
-    train_dataloader = OCRDataLoader(args.train_annotation_paths, 
-                                     args.image_height, 
-                                     args.image_width, 
-                                     table_path=args.table_path,
-                                     blank_index=BLANK_INDEX,
-                                     shuffle=True, 
-                                     batch_size=args.batch_size)
-    print("Num of training samples: {}".format(len(train_dataloader)))
-    if args.val_annotation_paths:
-        val_dataloader = OCRDataLoader(args.val_annotation_paths,
-                                       args.image_height,
-                                       args.image_width,
-                                       table_path=args.table_path,
-                                       blank_index=BLANK_INDEX,
-                                       batch_size=args.batch_size)
-        print("Num of val samples: {}".format(len(val_dataloader)))
-    print("Num of classes: {}".format(NUM_CLASSES))
-    print("Blank index is {}".format(BLANK_INDEX))
+    train_dl, val_dl = dataloader()
     localtime = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
     print("Start at {}".format(localtime))
 
     model = CRNN(NUM_CLASSES, args.backbone)
     model.summary()
-    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(args.learning_rate,
-                                                                 decay_steps=10000,
-                                                                 decay_rate=0.96,
-                                                                 staircase=True)
+    lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+        args.learning_rate,
+        decay_steps=10000,
+        decay_rate=0.96,
+        staircase=True)
     optimizer = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
 
     checkpoint = tf.train.Checkpoint(model=model, optimizer=optimizer)
     if args.checkpoint:
         localtime = args.checkpoint.rstrip("/").split("/")[-1]
-    manager = tf.train.CheckpointManager(checkpoint, directory="./tf_ckpts/{}".format(localtime), max_to_keep=args.max_to_keep)
-    summary_writer = tf.summary.create_file_writer("./logs/{}".format(localtime))
+    manager = tf.train.CheckpointManager(
+        checkpoint, 
+        directory="tf_ckpts/{}".format(localtime), 
+        max_to_keep=args.max_to_keep)
+    
     checkpoint.restore(manager.latest_checkpoint)
     if manager.latest_checkpoint:
         print("Restored from {}".format(manager.latest_checkpoint))
     else:
         print("Initializing from scratch")
 
-    avg_loss = tf.keras.metrics.Mean(name="train_loss")
-    val_avg_loss = tf.keras.metrics.Mean(name="val_loss")
+    train_summary_writer = tf.summary.create_file_writer(
+        f"logs/{localtime}/train")
+    val_summary_writer = tf.summary.create_file_writer(
+        f"logs/{localtime}/val")
+
+    avg_loss = tf.keras.metrics.Mean(name="loss")
 
     for epoch in range(1, args.epochs + 1):
-        with summary_writer.as_default():
-            for X, Y in train_dataloader():
-                loss = train_one_step(model, X, Y, optimizer)
-                tf.summary.scalar("train_loss", loss, step=optimizer.iterations)
+        with train_summary_writer.as_default():
+            for x, y in train_dl():
+                loss = train_one_step(model, optimizer, x, y)
                 avg_loss.update_state(loss)
-            print("[{} / {}] Mean train loss: {}".format(epoch, args.epochs, avg_loss.result()))
-            avg_loss.reset_states()
-            if (epoch - 1) % args.save_freq == 0:
-                saved_path = manager.save(checkpoint_number=epoch)
-                print("Model saved to {}".format(saved_path))
-                if args.val_annotation_paths:
-                    num_correct_samples = 0
-                    for X, Y in val_dataloader():
-                        decoded, loss = val_one_step(model, X, Y)
-                        count = map_and_count(decoded, Y, INT_TO_CHAR)
-                        val_avg_loss.update_state(loss)
-                        num_correct_samples += count
-                    tf.summary.scalar("val_loss", val_avg_loss.result(), step=epoch)
-                    tf.summary.scalar("val_accuracy(line, greedy decoder)", num_correct_samples / len(val_dataloader), step=epoch)
-                    print("[{} / {}] Mean val loss: {}".format(epoch, args.epochs, val_avg_loss.result()))
-                    print("[{} / {}] Accuracy(line, greedy decoder): {:.2f}".format(epoch, args.epochs, num_correct_samples / len(val_dataloader)))
-                    val_avg_loss.reset_states()
+                tf.summary.scalar("loss", loss, optimizer.iterations)
+            tf.summary.scalar("avg_loss", avg_loss.result(), epoch)
+        avg_loss.reset_states()
+        if not (epoch - 1) % args.save_freq:
+            checkpoint_path = manager.save(checkpoint_number=epoch)
+            print(f"Model saved to {checkpoint_path}")
+            if val_dl is not None:
+                num_correct_samples = 0
+                with val_summary_writer.as_default():
+                    for x, y in val_dl():
+                        loss, decoded = val_one_step(model, x, y)
+                        cnt = map_and_count(decoded, y, INT_TO_CHAR)
+                        avg_loss.update_state(loss)
+                        num_correct_samples += cnt
+                    tf.summary.scalar("avg_loss", avg_loss.result(), epoch)
+                    tf.summary.scalar("val_accuracy(line, greedy decoder)",
+                                      num_correct_samples / len(val_dl) * 100,
+                                      step=epoch)
+                avg_loss.reset_states()
+        
