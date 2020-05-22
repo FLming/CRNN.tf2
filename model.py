bbe2924ea0f4f7b3c364898390175674a1f7d5d9
FLming <yiminghd@foxmail.com>
2020-05-22 07:17:01 +0000

    Update model structure to ieee version

--- model.py
+++ model.py
@@ -4,12 +4,12 @@ from tensorflow.keras import layers
 
 def build_model(num_classes, image_width=None, channels=1):
     """
-    build cnn-rnn model
+    build CNN-RNN model
     """
-    def original(input_tensor):
+    def vgg_style(input_tensor):
         """
         The original feature extraction structure from CRNN paper.
-        Related paper: http://arxiv.org/abs/1507.05717
+        Related paper: https://ieeexplore.ieee.org/abstract/document/7801919
         """
         x = layers.Conv2D(
             filters=64, 
@@ -25,24 +25,29 @@ def build_model(num_classes, image_width=None, channels=1):
             activation='relu')(x)
         x = layers.MaxPool2D(pool_size=2, padding='same')(x)
 
-        for i in range(2):
-            x = layers.Conv2D(filters=256, kernel_size=3, padding='same',
-                              activation='relu')(x)
+        x = layers.Conv2D(filters=256, kernel_size=3, padding='same')(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('relu')(x)
+        x = layers.Conv2D(filters=256, kernel_size=3, padding='same',
+                          activation='relu')(x)
         x = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 1), 
                              padding='same')(x)
 
-        for i in range(2):
-            x = layers.Conv2D(filters=512, kernel_size=3, padding='same')(x)
-            x = layers.BatchNormalization()(x)
-            x = layers.Activation('relu')(x)
+        x = layers.Conv2D(filters=512, kernel_size=3, padding='same')(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('relu')(x)
+        x = layers.Conv2D(filters=512, kernel_size=3, padding='same',
+                          activation='relu')(x)
         x = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 1), 
                              padding='same')(x)
 
-        x = layers.Conv2D(filters=512, kernel_size=2, activation='relu')(x)
+        x = layers.Conv2D(filters=512, kernel_size=2)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Activation('relu')(x)
         return x
 
     img_input = keras.Input(shape=(32, image_width, channels))
-    x = original(img_input)
+    x = vgg_style(img_input)
     x = layers.Reshape((-1, 512))(x)
 
     x = layers.Bidirectional(layers.LSTM(units=256, return_sequences=True))(x)
