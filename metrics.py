00dc26ce00808fd0945f491e1202f223837bc661
FLming <yiminghd@foxmail.com>
2020-07-07 05:00:09 +0000

    Modified to support distributed training.

--- metrics.py
+++ metrics.py
@@ -8,32 +8,27 @@ class WordAccuracy(keras.metrics.Metric):
     """
     def __init__(self, name='word_accuracy', **kwargs):
         super().__init__(name=name, **kwargs)
-        self.total = self.add_weight(name='total', dtype=tf.int32, 
-                                     initializer=tf.zeros_initializer())
-        self.count = self.add_weight(name='count', dtype=tf.int32, 
-                                     initializer=tf.zeros_initializer())
+        self.total = self.add_weight(name='total', initializer='zeros')
+        self.count = self.add_weight(name='count', initializer='zeros')
                 
     def update_state(self, y_true, y_pred, sample_weight=None):
-        """
-        Maybe have more fast implementation.
-        """
-        b = tf.shape(y_true)[0]
+        batch_size = tf.shape(y_true)[0]
         max_width = tf.maximum(tf.shape(y_true)[1], tf.shape(y_pred)[1])
         logit_length = tf.fill([tf.shape(y_pred)[0]], tf.shape(y_pred)[1])        
         decoded, _ = tf.nn.ctc_greedy_decoder(
             inputs=tf.transpose(y_pred, perm=[1, 0, 2]),
             sequence_length=logit_length)
-        y_true = tf.sparse.reset_shape(y_true, [b, max_width])
-        y_pred = tf.sparse.reset_shape(decoded[0], [b, max_width])
+        y_true = tf.sparse.reset_shape(y_true, [batch_size, max_width])
+        y_pred = tf.sparse.reset_shape(decoded[0], [batch_size, max_width])
         y_true = tf.sparse.to_dense(y_true, default_value=-1)
         y_pred = tf.sparse.to_dense(y_pred, default_value=-1)
-        y_true = tf.cast(y_true, tf.int32)
-        y_pred = tf.cast(y_pred, tf.int32)
+        y_pred = tf.cast(y_pred, tf.float32)
         values = tf.math.reduce_any(tf.math.not_equal(y_true, y_pred), axis=1)
-        values = tf.cast(values, tf.int32)
+        values = tf.cast(values, tf.float32)
         values = tf.reduce_sum(values)
-        self.total.assign_add(b)
-        self.count.assign_add(b - values)
+        batch_size = tf.cast(batch_size, tf.float32)
+        self.total.assign_add(batch_size)
+        self.count.assign_add(batch_size - values)
 
     def result(self):
         return self.count / self.total
