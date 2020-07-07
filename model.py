282b025735d03200b41dd34f146336b602a8ce49
FLming <yiminghd@foxmail.com>
2020-07-07 05:06:17 +0000

    Make vgg_style a normal function.

--- model.py
+++ model.py
@@ -2,49 +2,37 @@ from tensorflow import keras
 from tensorflow.keras import layers
 
 
+def vgg_style(input_tensor):
+    """
+    The original feature extraction structure from CRNN paper.
+    Related paper: https://ieeexplore.ieee.org/abstract/document/7801919
+    """
+    x = layers.Conv2D(64, 3, padding='same', activation='relu')(input_tensor)
+    x = layers.MaxPool2D(pool_size=2, padding='same')(x)
+
+    x = layers.Conv2D(128, 3, padding='same', activation='relu')(x)
+    x = layers.MaxPool2D(pool_size=2, padding='same')(x)
+
+    x = layers.Conv2D(256, 3, padding='same', use_bias=False)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.Conv2D(256, 3, padding='same', activation='relu')(x)
+    x = layers.MaxPool2D(pool_size=2, strides=(2, 1), padding='same')(x)
+
+    x = layers.Conv2D(512, 3, padding='same', use_bias=False)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    x = layers.Conv2D(512, 3, padding='same', activation='relu')(x)
+    x = layers.MaxPool2D(pool_size=2, strides=(2, 1), padding='same')(x)
+
+    x = layers.Conv2D(512, 2, use_bias=False)(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.Activation('relu')(x)
+    return x
+
+
 def build_model(num_classes, image_width=None, channels=1):
-    """
-    build CNN-RNN model
-    """
-    def vgg_style(input_tensor):
-        """
-        The original feature extraction structure from CRNN paper.
-        Related paper: https://ieeexplore.ieee.org/abstract/document/7801919
-        """
-        x = layers.Conv2D(
-            filters=64, 
-            kernel_size=3, 
-            padding='same',
-            activation='relu')(input_tensor)
-        x = layers.MaxPool2D(pool_size=2, padding='same')(x)
-
-        x = layers.Conv2D(
-            filters=128, 
-            kernel_size=3, 
-            padding='same',
-            activation='relu')(x)
-        x = layers.MaxPool2D(pool_size=2, padding='same')(x)
-
-        x = layers.Conv2D(filters=256, kernel_size=3, padding='same')(x)
-        x = layers.BatchNormalization()(x)
-        x = layers.Activation('relu')(x)
-        x = layers.Conv2D(filters=256, kernel_size=3, padding='same',
-                          activation='relu')(x)
-        x = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 1), 
-                             padding='same')(x)
-
-        x = layers.Conv2D(filters=512, kernel_size=3, padding='same')(x)
-        x = layers.BatchNormalization()(x)
-        x = layers.Activation('relu')(x)
-        x = layers.Conv2D(filters=512, kernel_size=3, padding='same',
-                          activation='relu')(x)
-        x = layers.MaxPool2D(pool_size=(2, 2), strides=(2, 1), 
-                             padding='same')(x)
-
-        x = layers.Conv2D(filters=512, kernel_size=2)(x)
-        x = layers.BatchNormalization()(x)
-        x = layers.Activation('relu')(x)
-        return x
+    """build CNN-RNN model"""
 
     img_input = keras.Input(shape=(32, image_width, channels))
     x = vgg_style(img_input)
