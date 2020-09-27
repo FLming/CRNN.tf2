dce2c9210077c47cb2f439b1c2e49aada9cab880
FLming <yiminghd@foxmail.com>
2020-09-27 09:38:09 +0000

    copy the config file to model dir

--- crnn/train.py
+++ crnn/train.py
@@ -1,5 +1,5 @@
 import argparse
-import time
+import shutil
 from pathlib import Path
 
 import yaml
@@ -12,23 +12,23 @@ from losses import CTCLoss
 from metrics import WordAccuracy
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--config', type=str, required=True, 
+parser.add_argument('--config', type=Path, required=True, 
                     help='The config file path.')
-parser.add_argument('--model_dir', type=str, required=True,
+parser.add_argument('--model_dir', type=Path, required=True,
                     help='The path to save the model and log')
 args = parser.parse_args()
 
-with open(args.config) as f:
+with args.config.open() as f:
     config = yaml.load(f, Loader=yaml.Loader)['train']
 
-localtime = time.asctime()
-model_dir = Path(args.model_dir, localtime)
-model_dir.mkdir()
+args.model_dir.mkdir(exist_ok=True)
+if list(args.model_dir.iterdir()):
+    raise ValueError(f'{args.model_dir} is not a empty folder')
+shutil.copy(args.config, args.model_dir / args.config.name)
 model_prefix = '{epoch}_{word_accuracy:.4f}_{val_word_accuracy:.4f}'
-saved_model_path = f'{model_dir}/{model_prefix}.h5'
+saved_model_path = f'{args.model_dir}/{model_prefix}.h5'
 strategy = tf.distribute.MirroredStrategy()
 batch_size = config['batch_size_per_replica'] * strategy.num_replicas_in_sync
-print('Training start at {}'.format(localtime))
 
 dataset_builder = DatasetBuilder(**config['dataset_builder'])
 train_ds = dataset_builder.build(config['train_ann_paths'], batch_size, True)
@@ -47,7 +47,7 @@ callbacks = [
     keras.callbacks.ModelCheckpoint(saved_model_path),
     keras.callbacks.ReduceLROnPlateau(monitor='val_word_accuracy', mode='max',
                                       **config['reduce_lr']),
-    keras.callbacks.TensorBoard(log_dir=str(model_dir), 
+    keras.callbacks.TensorBoard(log_dir=str(args.model_dir), 
                                 **config['tensorboard'])]
 
 model.fit(train_ds, epochs=config['epochs'], callbacks=callbacks,
