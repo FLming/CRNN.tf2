30e72cbc63a284fb04de7018c6fc7d0fa1abe9a1
FLming <yiminghd@foxmail.com>
2021-01-22 06:09:58 +0000

    better post add way

--- tools/export.py
+++ tools/export.py
@@ -21,18 +21,17 @@ with args.config.open() as f:
     config = yaml.load(f, Loader=yaml.Loader)['dataset_builder']
 
 trained_model = keras.models.load_model(args.model, compile=False)
-if args.post:
-    img_input = keras.Input(shape=config['img_shape'])
-    x = trained_model(img_input)
-    if args.post == 'softmax':
-        x = tf.nn.softmax(x, axis=-1)
-    elif args.post == 'greedy':
-        x = decoders.CTCGreedyDecoder(config['table_path'])(x)
-    elif args.post == 'beam_search':
-        x = decoders.CTCBeamSearchDecoder(config['table_path'])(x)
-    model = keras.Model(inputs=img_input, outputs=x)
+img_input = trained_model.inputs[0]
+x = trained_model.outputs[0]
+if args.post == 'softmax':
+    x = keras.layers.Softmax()(x)
+elif args.post == 'greedy':
+    x = decoders.CTCGreedyDecoder(config['table_path'])(x)
+elif args.post == 'beam_search':
+    x = decoders.CTCBeamSearchDecoder(config['table_path'])(x)
 else:
-    model = trained_model
+    pass
+model = keras.Model(inputs=img_input, outputs=x)
 
 model.summary()
 
